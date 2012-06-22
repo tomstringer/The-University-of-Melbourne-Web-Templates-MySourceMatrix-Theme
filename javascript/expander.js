2ba7f1dd9943ee0c586074942a31f9cafddfcee6
Tom Stringer <t.stringer@unimelb.edu.au>
2012-06-22 17:01:59 +1000

    changed expand-title and expand-content to question and answer

--- javascript/expander.js
+++ javascript/expander.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 	//style all questions as closed
-	$(".expand-title").addClass("closed"); 
-	$(".expand-title").append(" <a class='more'>More</a>");
+	$(".question").addClass("closed"); 
+	$(".question").append(" <a class='more'>More</a>");
 	//make sure first question is styled as open
 	//$(".question:first").removeClass("closed").addClass("opened"); 
-	$(".expand-content").hide(); //hide answers
+	$(".answer").hide(); //hide answers
 	// $(".answer:first").show(); //show first answer
 	//question click
-	$(".expand-title").click(function() {
-		$(".expand-content").slideUp("fast");
-		$(".expand-title").removeClass("opened").addClass("closed");
-		$(".expand-title.closed a").replaceWith("<a class='more'>More</a>");
+	$(".question").click(function() {
+		$(".answer").slideUp("fast");
+		$(".question").removeClass("opened").addClass("closed");
+		$(".question.closed a").replaceWith("<a class='more'>More</a>");
 		
-		if ($(this).next(".expand-content").is(":hidden")) {
-			$(this).next(".expand-content").slideDown("fast");
+		if ($(this).next(".answer").is(":hidden")) {
+			$(this).next(".answer").slideDown("fast");
 			$(this).removeClass("closed").addClass("opened");
-			$(".expand-title.opened a").replaceWith("<a class='less'>Less</a>");
+			$(".question.opened a").replaceWith("<a class='less'>Less</a>");
 		}              
 	});
 });
